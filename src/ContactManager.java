3152aed9a9e0dbc4e1810cecc481cb1d91d9fe29
Stephanie <stepdugas@gmail.com>
2025-03-03 19:02:23 -0500

    Commit ContactManager

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -1,2 +1,20 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class ContactManager {
+
+    //Make a hashmap to store contacts (name and phone number)
+    HashMap<String, String> contacts = new HashMap<>();
+
+    //Make a scanner to get user input
+    Scanner scanner = new Scanner(System.in);
+
+
+    //a main method to run the program
+    public static void main(String[] args) {
+
+    }
+
+
+
 }
